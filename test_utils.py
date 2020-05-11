b48ab5084f2066506e9fbc703e39bb17c933b15e
Luisa Brüser <luisa.brueser@gmail.com>
2020-05-11 18:20:55 +0200

    Update utils+test with temp controller

--- test_utils.py
+++ test_utils.py
@@ -1,4 +1,15 @@
-from utils import funny
+from utils import conversion_values, temperature_controller
 
-def test_funny(): 
-    assert funny (2, 1) == 3
+def test_conversion():
+    assert conversion_values(True, 25, 28, 8320) [0] == 26
+    assert conversion_values(True, 25, 28, 8320) [1] == 25
+
+
+outputs_conversion = conversion_values(True, 25, 28, 8320)
+temp=outputs_conversion[0]
+temp_should=outputs_conversion[1]
+
+    
+def test_controller():
+    assert temperature_controller(True, temp, temp_should, 0.5) == False
+  
--- utils.py
+++ utils.py
@@ -1,2 +1,42 @@
-def funny(x,y):
-    return x + y
+# Measured Temperature from Sensor, Input variable
+#temp_measured = 2000 
+# Output variable to heating Unit, True if on, False if Heating Off
+#heating_ON = False
+
+# Definition of tempertures
+#tempV1 = 25
+#tempV2 = 28
+#temp_should = 0
+
+
+# Definition of hysteresis 
+#temp_range= 0.5
+
+# Chosing construction model (2 different models in the laboratory), If VT1 choose true, if VT2 choose false!
+#VT_1 = True
+
+#Conversions between the PLC analog input data and the physical measured values
+
+def conversion_values(VT_1, tempV1,tempV2, temp_measured):
+    if VT_1 == True:
+        temp_should = tempV1
+        temp = temp_measured/320
+    else:
+        temp_should = tempV2
+        temp = temp_measured / (100 + 10 / 3)
+    return [temp, temp_should]
+
+
+def temperature_controller(heating_ON, temp, temp_should, temp_range):
+    if  heating_ON == False and temp < (temp_should - temp_range):
+        heating_ON = True
+    elif heating_ON == True and temp > (temp_should + temp_range):
+        heating_ON = False
+    return heating_ON
+
+outputs_conversion = conversion_values(True, 25, 28, 8320)
+
+temp=outputs_conversion[0]
+temp_should=outputs_conversion[1]
+
+temperature_controller(True, temp, temp_should, 0.5)
